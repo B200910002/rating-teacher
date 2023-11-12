918bd5d7ff7867836d94c109640796533ab12fbf
Jamts Budee <jamts.b@nomadicss.mn>
2023-11-12 19:33:47 +0800

    Initial version of ratingteacher generated by generator-jhipster@7.9.4

---
+++ src/main/java/com/ocean/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ocean.repository;
+
+import com.ocean.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/com/ocean/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.ocean;
+
+import com.ocean.RatingteacherApp;
+import com.ocean.config.AsyncSyncConfiguration;
+import com.ocean.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { RatingteacherApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}

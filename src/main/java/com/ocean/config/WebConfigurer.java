99e7dfb526e4776e30f5184b598ea94a62679b4b
Mundone <munkhochir113@gmail.com>
2023-12-07 23:54:10 +0800

    changed cors

--- src/main/java/com/ocean/config/WebConfigurer.java
+++ src/main/java/com/ocean/config/WebConfigurer.java
@@ -47,7 +47,7 @@ public class WebConfigurer implements ServletContextInitializer {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3032")); // Add other origins if needed
+        config.setAllowedOrigins(Arrays.asList("*")); // This allows all origins
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);

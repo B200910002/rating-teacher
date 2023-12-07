55e2d9c93a095b1cb4be94d4e75b4730f8be56cb
Mundone <munkhochir113@gmail.com>
2023-12-08 01:06:03 +0800

    changed cors

--- src/main/java/com/ocean/web/rest/UserJWTController.java
+++ src/main/java/com/ocean/web/rest/UserJWTController.java
@@ -3,7 +3,10 @@ package com.ocean.web.rest;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ocean.security.jwt.JWTFilter;
 import com.ocean.security.jwt.TokenProvider;
+import com.ocean.service.StudentService;
+import com.ocean.service.dto.StudentDTO;
 import com.ocean.web.rest.vm.LoginVM;
+import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -26,9 +29,16 @@ public class UserJWTController {
 
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public UserJWTController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
+    private final StudentService studentService; // Add StudentService
+
+    public UserJWTController(
+        TokenProvider tokenProvider,
+        AuthenticationManagerBuilder authenticationManagerBuilder,
+        StudentService studentService
+    ) {
         this.tokenProvider = tokenProvider;
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.studentService = studentService; // Initialize StudentService
     }
 
     @CrossOrigin(origins = "*")
@@ -45,6 +55,47 @@ public class UserJWTController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
         return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
+    }
+
+    @GetMapping("/get_auth_info")
+    public ResponseEntity<?> getAuthInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName(); // Assuming username is the student code
+
+        // Fetch student info based on username (student code)
+        Optional<StudentDTO> studentDTO = studentService.findOneByStudentCode(username);
+
+        if (studentDTO.isPresent()) {
+            // Assuming the token is still valid and you have access to it
+            String token = ""; // Extract the token from the SecurityContext or Authentication object
+
+            // Create a response object containing student info and token
+            AuthInfoResponse response = new AuthInfoResponse(studentDTO.get(), token);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Class for response
+    static class AuthInfoResponse {
+
+        private StudentDTO studentInfo;
+        private String token;
+
+        public AuthInfoResponse(StudentDTO studentInfo, String token) {
+            this.studentInfo = studentInfo;
+            this.token = token;
+        }
+
+        // Getters
+        public StudentDTO getStudentInfo() {
+            return studentInfo;
+        }
+
+        public String getToken() {
+            return token;
+        }
     }
 
     /**
